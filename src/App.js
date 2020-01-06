6b16c65c6b1a1d1b1a51c0a1fc50c02a775c8ee5
administrator <admin@administrators-MacBook-Air.local>
2020-01-06 16:59:42 +0000

    add six titles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import batmanCover from './batman.jpg';
+import logo from './kodflix-logo.png';
 import './App.css';
 
 
 function App() {
   return (
     <div className="App">
-      <img alt="batman movie cover" className="movieCover" src={batmanCover} />
+      <img alt="kodflix logo" className="logo" src={logo} />
+      <div className="container">
+        <div className="movieCover">Batman</div>
+        <div className="movieCover">Avatar</div>
+        <div className="movieCover">Johnny Mnemonic</div>
+      </div>
+      <div className="container">
+        <div className="movieCover">Matrix</div>
+        <div className="movieCover">The Walking Dead</div>
+        <div className="movieCover">Game of Thrones</div>
+      </div>
     </div>
   );
 }
