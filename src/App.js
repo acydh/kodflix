844f5d8c80528c85a616e8dba05f7ae28a733dd3
administrator <admin@administrators-MacBook-Air.local>
2020-01-07 14:35:06 +0000

    fix issue #4

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './kodflix-logo.png';
-import './App.css';
 import './reset.css';
+import './App.css';
 import batmanCover from './batman.jpg';
 import avatarCover from './avatar.jpg';
 import johnnyMnemonicCover from './johnnymnemonic.jpg';
@@ -15,14 +15,14 @@ function App() {
     <div className="App">
       <img alt="kodflix logo" className="logo" src={logo} />
       <div className="container">
-        <div className="movieCover"><img src={batmanCover} /></div>
-        <div className="movieCover"><img src={avatarCover} /></div>
-        <div className="movieCover"><img src={johnnyMnemonicCover} /></div>
+        <div className="movieCover"><img src={batmanCover} alt="batman cover" /></div>
+        <div className="movieCover"><img src={avatarCover} alt="avatar cover" /></div>
+        <div className="movieCover"><img src={johnnyMnemonicCover} alt="johnny mnemonic cover" /></div>
       </div>
       <div className="container">
-        <div className="movieCover"><img src={matrixCover} /></div>
-        <div className="movieCover"><img src={thewalkingdeadCover} /></div>
-        <div className="movieCover"><img src={gameofthronesCover} /></div>
+        <div className="movieCover"><img src={matrixCover} alt="matrix cover" /></div>
+        <div className="movieCover"><img src={thewalkingdeadCover} alt="the walking dead cover" /></div>
+        <div className="movieCover"><img src={gameofthronesCover} alt="game of thrones cover" /></div>
       </div>
     </div>
   );
