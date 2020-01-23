d7c0418c6c7af170a7ae3a0e03909b7bfe5b7b8d
administrator <admin@administrators-MacBook-Air.local>
2020-01-23 16:16:00 +0000

    fix merge back and front end for deployment

--- src/backend/app.js
+++ src/backend/app.js
@@ -3,14 +3,14 @@ const app = express();
 const port = process.env.PORT || 3001;
 const path = require('path');
 
-app.use(express.static(path.join(__dirname, 'build')))
+app.use(express.static(path.join(__dirname, '../../build')));
 
 app.get('/rest/shows', (req, res) => {
     res.sendFile('shows.json', { root: __dirname });
 });
 
 app.get('*', function (req, res) {
-    res.sendFile('index.html', { root: "build" });
+    res.sendFile(path.join(__dirname, '../../build', 'index.html'));
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
